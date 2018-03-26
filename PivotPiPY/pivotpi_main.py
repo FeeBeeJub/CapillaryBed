a5486fb445c33cef0d5f96554edbee2709859e58
Phoebe Welch <pwelch@buffalo.edu>
2018-03-25 20:27:21 -0400

    Phoebe's changes

--- PivotPiPY/pivotpi_main.py
+++ PivotPiPY/pivotpi_main.py
@@ -9,18 +9,33 @@ import threading
 import pivotpi_servo
 import pivotpi
 
+
 if __name__ == '__main__':
-    servoName = "Pulse_0"
+    servoNames = {}
+    servoNames[0] = "Pulse_0"
+    servoNames[1] = "Pulse_1"
+    minPos = {}
+    minPos[0] = 0
+    minPos[1] = 0
+    maxPos = {}
+    maxPos[0] = 120
+    maxPos[2] = 120
+    
     ppi = pivotpi.PivotPi(addr = 0x40, actual_frequency = 60)
     ppilock = threading.Lock()
     channel = 0
-    minPos = 20
-    maxPos = 160
-    pauseAtMin = 0.5
-    pauseAtMax = 0.5
-    ppst = pivotpi_servo.PivotPIServoThread(servoName, ppi, ppilock, channel, minPos, maxPos, pauseAtMin, pauseAtMax)
+    pauseAtMin = {}
+    pauseAtMin[0] = 0.5
+    pauseAtMin[1] = 0.5
+    pauseAtMax = {}
+    pauseAtMax[0] = 0.5
+    pauseAtMax[1] = 0.5
     
-    ppst.start()
+    ppst = {}
+    
+    for i in range(2):
+        ppst[i] = pivotpi_servo.PivotPIServoThread(servoNames[i], ppi, ppilock, i, minPos[i], maxPos[i], pauseAtMin[i], pauseAtMax[i])
+    ppst[i].start()
     
     np = eval( input("Input any integer to terminate:  ") )
     
